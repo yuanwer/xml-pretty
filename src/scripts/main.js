d88061f664b16dd29ff85444ec741cbbb160b0bd
wangyaomin <2856572867@qq.com>
2023-05-15 18:39:55 +0800

    feat: 替换XML格式化工具为vkbeautify，优化美化格式

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,31 +6,39 @@ import 'monaco-editor/esm/vs/basic-languages/xml/xml.contribution.js'
 import xmlFormatter from 'xml-formatter'
 
 // 创建编辑器实例
-const editor = monaco.editor.create(document.getElementById('e"editor" {
-  value: ''""  language: 'x"xml"  theme: 'v"vs-dark"  fontSize: 18,
-)
+const editor = monaco.editor.create(document.getElementById('editor'), {
+  value: '',
+  language: 'xml',
+  theme: 'vs-dark',
+  fontSize: 18,
+})
 
-;// 格式化 XML
+// 格式化 XML
 const formatXml = (originalText) => {
-  const xmlDoc = new DOMParser().parseFromString(originalText, 't"text/xml" ; const xmlStr = new XMLSerializer().serializeToString(xmlDoc)
- ; return vkbeautify.xml(xmlStr)
-};
+  const xmlDoc = new DOMParser().parseFromString(originalText, 'text/xml')
+  const xmlStr = new XMLSerializer().serializeToString(xmlDoc)
+  return vkbeautify.xml(xmlStr)
+}
 
-;function openGithub() {
-  window.open('h"https://github.com/yuanwer/xml-pretty"};
+function openGithub() {
+  window.open('https://github.com/yuanwer/xml-pretty')
+}
 
 function compressXml(originalText) {
   return originalText
-};
+}
 
-const $formatBtn = document.getElementById('f"formatBtn"$;formatBtn.addEventListener('c"click"() => {
+const $formatBtn = document.getElementById('formatBtn')
+$formatBtn.addEventListener('click', () => {
   const originalText = editor.getValue()
- ; editor.setValue(formatXml(originalText))
-};)
+  editor.setValue(formatXml(originalText))
+})
 
-;const $githubBtn = document.getElementById('g"githubBtn"$;githubBtn.addEventListener('c"click"openGithub)
+const $githubBtn = document.getElementById('githubBtn')
+$githubBtn.addEventListener('click', openGithub)
 
-;const $compressBtn = document.getElementById('c"compressBtn"$;compressBtn.addEventListener('c"click"() => {
+const $compressBtn = document.getElementById('compressBtn')
+$compressBtn.addEventListener('click', () => {
   const originalText = editor.getValue()
- ; editor.setValue(compressXml(originalText))
-};)
+  editor.setValue(compressXml(originalText))
+})
